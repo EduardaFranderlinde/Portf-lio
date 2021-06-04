07039c22484cdddc5c952c0dd1655f96d67ea769
EduardaFranderlinde <83764701+EduardaFranderlinde@users.noreply.github.com>
2021-06-04 09:51:21 -0300

    Foi inserido o script.

--- Fundamentos de TI/Exemplos/valor_positivo_negativo.sh
+++ Fundamentos de TI/Exemplos/valor_positivo_negativo.sh
@@ -4,3 +4,13 @@ Eduarda Garcia Franderlinde
 # Descrição: 
 O script lê o valor inserido e determina se ele é positvo ou negativo.
 
+#!/bin/bash
+echo "Digite um número"
+read x
+
+if [ $x -lt 0 ]
+then
+  echo "O número digita é positivo!"
+else 
+  echo "O número digitado é negativo!"
+fi
