cbb76fa93529310afe415439c60badb29ff916b4
EduardaFranderlinde <83764701+EduardaFranderlinde@users.noreply.github.com>
2021-06-09 14:57:22 -0300

    Inserção do código

---
+++ Fundamentos de TI/Exemplos/ordem_crescente.sh
@@ -0,0 +1,13 @@
+#autor(a):
+Eduarda Garcia Franderlinde
+
+#Descrição:
+Algoritmo que imprimir os números de 1 a 10 em ordem crscente.
+
+#!/bin/bash
+i=1
+while [ $i -lt 11 ]
+do
+ echo "- $i"
+ i=$((i+1))
+done
