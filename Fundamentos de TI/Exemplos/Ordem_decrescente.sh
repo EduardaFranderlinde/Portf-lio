916c7a26c123202a46b3dde251b29cc541be8563
EduardaFranderlinde <83764701+EduardaFranderlinde@users.noreply.github.com>
2021-06-09 14:59:59 -0300

    Inserção de informações.

---
+++ Fundamentos de TI/Exemplos/Ordem_decrescente.sh
@@ -0,0 +1,13 @@
+#Autor(a):
+Eduarda Garcia Franderlinde
+
+#Descrição:
+Algoritmo que imprimi os números de 1 a 10 em ordem decrescente.
+
+#!/bin/bash
+i=10
+while [ $i -gt 0 ]
+do
+ echo "- $i"
+ i=$((i-1))
+done
