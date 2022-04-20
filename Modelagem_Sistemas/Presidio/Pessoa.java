2d5c65d26e4a0bb99eb9e7ff0e44e1784ca3d2a5
EduardaFranderlinde <83764701+EduardaFranderlinde@users.noreply.github.com>
2022-04-20 10:59:37 -0300

    Update Pessoa.java

--- Modelagem_Sistemas/Presidio/Pessoa.java
+++ Modelagem_Sistemas/Presidio/Pessoa.java
@@ -1,23 +1,22 @@
-import java.util.ArrayList;
-public class Pavilhao{
-  private int cela;
-  private int refeitorio;
-  private int banheiro;
-  private int solitaria;
-  private ArrayList<Presidiaro> Presidiario;
-  private ArrayList<Funcionario> Funcionario;
-  private int id;
-  private String tipo;
-  private int patio;
-  private int biblioteca;
-  private int lavanderia;
-  private int salaAula;
-  private ArrayList<String> salaTecnico;
-  private int capela;
-  private ArrayList<String> 
-  private boolean lojaConveniencia;
+public class Pessoa{
+  private String nome;
+  private String cpf;
+  private String data_nasc;
+  private String contato;
+  private String genero;
+  private String endereco;
 
-  public Pavilhao (int cela, int refeitorio, int banheiro, int solitaria, int id, String tipo, int patio, int biblioteca, int lavanderia, int salaAula, int capela, boolean lojaConveniencia){
-    
-  }
+
+public Pessoa(String nome, String cpf, String data_nasc, String contato, String genero, String endereco){
+  
 }
+  
+public void registrar(String nome, String cpf, String data_nasc, String contato, String genero, String endereco){
+  
+}
+
+public boolean verificarPresenca(String nome, String cpf){
+  return true;
+}
+
+  }
