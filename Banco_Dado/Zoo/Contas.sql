38c4da6a99647fac1aa09e1aaf9eab98097e7f99
EduardaFranderlinde <83764701+EduardaFranderlinde@users.noreply.github.com>
2022-04-20 11:40:38 -0300

    Create Contas.sql

---
+++ Banco_Dado/Zoo/Contas.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Contas(
+Cod int primary key,
+Lucro decimal(12,2),
+Valor_total_bilheteria decimal(11,2),
+Valor_total_estoque decimal(11,2),
+Valor_total_fun decimal(11,2),
+Gastos_extras decimal(11,2),
+Mes_ano varchar(20)
+);
+
+--Insert pelo import--
